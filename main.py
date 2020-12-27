59fb3ba4a6f8a4c9947cd5d3b7a2245a31ee8619
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2020-12-27 09:05:00 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import bs4 as BeautifulSoup
+import requests
+import xmlify
+import geckodriver_autoinstaller
+import time
+import ctypes
+url = 'https://github.com/lalarcon-py'
+driver = webdriver.Firefox()
+driver.get('https://github.com/lalarcon-py')
+soup = BeautifulSoup(response.txt, 'lxml')
+is_updated = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[47]/rect[4]').append(updated2)
+nonUpdated = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[53]/rect[5]')
+updated2 = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[47]/rect[4]')
+is_updated = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[47]/rect[4]').append(updated2)
+if updated1 or updated2:
+    time.sleep(60)
+else:
+    ctypes.windll.user32.MessageBoxW(0, "YOU HAVE NOT YET UPDATED YOUR GITHUB PAGE TODAY!!!", "ERROR")
+
+
+
+
+
+
