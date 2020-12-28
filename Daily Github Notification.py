bd00ca615f00308776e28dbfc60644db73e93297
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2020-12-27 20:55:41 -0800

    Update Daily Github Notification.py

--- Daily Github Notification.py
+++ Daily Github Notification.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-import bs4 as BeautifulSoup
-import xmlify
 import geckodriver_autoinstaller
 import time
 import ctypes
@@ -8,10 +6,8 @@ url = 'https://github.com/lalarcon-py'
 driver = webdriver.Firefox()
 driver.get('https://github.com/lalarcon-py')
 soup = BeautifulSoup(response.txt, 'lxml')
-is_updated = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[47]/rect[4]').append(updated2)
-nonUpdated = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[53]/rect[5]')
+updated1 = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[47]/rect[7]')
 updated2 = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[47]/rect[4]')
-is_updated = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[1]/svg/g/g[47]/rect[4]').append(updated2)
 while updated1 or updated2:
     time.sleep(60)
 else:
